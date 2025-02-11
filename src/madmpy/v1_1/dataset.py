4cc6c0f5265033eab0e167b88d6b9408760a5661
alberto <ballesterosbr@gmail.com>
2025-02-11 12:41:22 +0100

    initial release

--- src/madmpy/v1_1/dataset.py
+++ src/madmpy/v1_1/dataset.py
@@ -9,10 +9,17 @@ class Dataset(BaseModel):
     def to_dcat(self):
         """Returns a DCAT representation of the dataset"""
         lang = self.language if self.language else "und"
-        fragment ={"@id":"http://changeme/dataset1",
-                     "@type":["http://www.w3.org/ns/dcat#Dataset"],
-                     "http://purl.org/dc/terms/title":[{"@value":self.title,"@language":lang}],
-                     "http://purl.org/dc/terms/identifier":[{"@value":"http://changeme/dataset1"}],
-                     "http://purl.org/dc/terms/description":[{"@value":self.description,"@language":lang}],
+        fragment ={ "@id":"http://changeme/dataset1", # Preguntar a MA
+                    "@type":["http://www.w3.org/ns/dcat#Dataset"], # Preguntar a MA
+                    "http://purl.org/dc/terms/title":[{"@value":self.title,"@language":lang}],
+                    "http://purl.org/dc/terms/identifier":[{"@value":"http://changeme/dataset1"}],
+                    "http://purl.org/dc/terms/description":[{"@value":self.description,"@language":lang}],
+                    "http://purl.org/dc/elements/1.1/subject":[{"@value":self.keyword,"@language":lang}],
+                    "http://purl.org/dc/terms/language":[{"@value":self.language,"@language":lang}],
+                    "http://purl.org/dc/terms/issued":[{"@value":self.issued,"@language":lang}],
+                    "http://purl.org/dc/terms/type":[{"@value":self.type,"@language":lang}],
                     }
         return json.dumps(fragment)
+   
+
+# https://www.dublincore.org/specifications/dublin-core/dcmi-terms/
